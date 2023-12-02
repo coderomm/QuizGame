a91bbc337c0456e8fb1801ec1c3dbe4f28f69603
coder om <mail.coderom@gmail.com>
2023-12-02 17:34:47 +0530

    Add project files.

---
+++ QuizGame/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Quiz Game!");
+
+            // Define quiz questions and answers
+            Dictionary<string, string> quiz = new Dictionary<string, string>
+            {
+                { "What is the capital of France?", "Paris" },
+                { "Which planet is known as the Red Planet?", "Mars" },
+                { "What is the largest mammal in the world?", "Blue Whale" }
+            };
+
+            int score = 0;
+
+            // Iterate through quiz questions
+            foreach (var question in quiz)
+            {
+                Console.WriteLine("\n" + question.Key);
+                Console.Write("Your answer: ");
+                string userAnswer = Console.ReadLine();
+
+                // Check user's answer
+                if (string.Equals(userAnswer, question.Value, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Correct!\n");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. The correct answer is {question.Value}.\n");
+                }
+            }
+
+            // Display final score
+            Console.WriteLine($"Quiz completed! Your final score is: {score} out of {quiz.Count}");
+
+            // Ask user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgain = Console.ReadLine();
+
+            if (string.Equals(playAgain, "yes", StringComparison.OrdinalIgnoreCase))
+            {
+                Main(args); // Restart the game
+            }
+            else
+            {
+                Console.WriteLine("Thank you for playing!");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Quiz Game
+
+Welcome to the Quiz Game! Test your knowledge with a series of questions.
+
+## How to Play
+
+1. Clone the repository to your local machine.
+   ```bash
+   git clone https://github.com/coderomm/QuizGame.git
